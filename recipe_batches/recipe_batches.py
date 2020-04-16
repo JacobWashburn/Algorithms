a9c2387c7dcf47ade5331ef26c93ec83e63003d6
Jacob Washburn <jacobrwashburn@gmail.com>
2020-04-16 14:53:35 -0600

    recipe batches done and tests passing. pep8 reformat

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -1,14 +1,25 @@
 #!/usr/bin/python
 
-import math
 
 def recipe_batches(recipe, ingredients):
-  pass 
+    batches = 0
+    while True:
+        should_break = False
+        for i in recipe:
+            if i not in ingredients.keys() or recipe[i] > ingredients[i]:
+                should_break = True
+                break
+            ingredients[i] -= recipe[i]
+        if should_break:
+            break
+        batches += 1
+    return batches
 
 
 if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+    # Change the entries of these dictionaries to test
+    # your implementation with different inputs
+    recipe = {'milk': 100, 'flour': 4, 'sugar': 10, 'butter': 5}
+    ingredients = {'milk': 1288, 'flour': 9, 'sugar': 95}
+    print("{batches} batches can be made from the available ingredients: {ingredients}.".format(
+            batches = recipe_batches(recipe, ingredients), ingredients = ingredients))
